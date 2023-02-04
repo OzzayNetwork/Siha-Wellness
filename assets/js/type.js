21ae5899f473d6c6da464b07e5d0b0075ddbc8de
Kelvin Njuguna <ozzaynetwork@gmail.com>
2023-02-04 14:29:56 +0300

    kelvin: some changes

--- assets/js/type.js
+++ assets/js/type.js
@@ -4,3 +4,51 @@ window.ityped.init(document.querySelector('.ityped'),{
     backSpeed: 150,
     loop: true
 })
+
+$(document).ready(function(){
+
+    var packagesOwl = $('.owl-carousel');
+    
+    
+
+    packagesOwl.owlCarousel({
+        loop:true,
+        items:3,
+        margin:100,
+        center:true,
+        nav:false,
+        autoplay:true,
+        autoplayTimeout:8000,
+        autoplayHoverPause:true,
+        responsiveClass:true,
+        lazyLoad:true,
+        loop:true,
+        responsive:{
+            0:{
+                items:1,
+                
+            },
+            600:{
+                items:2,
+               
+            },
+            1000:{
+                items:3,
+                
+            }
+        }
+       
+    })
+
+     // Custom Button
+  $('.nav-next').click(function() {
+    packagesOwl.trigger('next.owl.carousel');
+  });
+  $('.nav-prev').click(function() {
+    packagesOwl.trigger('prev.owl.carousel');
+  });
+
+  $('products-nav .nav-link').on('click', function(){
+    $(this).addClass('active').parent().siblings().children().removeClass('active')
+  })
+  });
